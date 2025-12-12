754fdab2d0c09ec0e6aaff9befca12fc55c260a4
Johith Babu S <johithbabus@Johiths-MacBook-Pro.local>
2025-12-12 11:48:10 +0530

    first commit
    added bit_strings.cpp

---
+++ bit_strings.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+int main(){
+    ll n;
+    cin >> n;
+    ll res = 1;
+    res = res << n;
+    cout << res;
+    return 0;
+}
